20c04ccf01a0c4255fd52c580fe367d72ba8f536
ProObito <ahmedumaid707@gmail.com>
2025-05-21 19:52:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,3 +37,47 @@ class Config:
 
     # Admin mode
     ADMIN_MODE = False
+
+class Txt:
+    START_TXT = """<b>ʜᴇʏ! {}  
+
+» ɪ ᴀᴍ ᴀᴅᴠᴀɴᴄᴇᴅ ʀᴇɴᴀᴍᴇ ʙᴏᴛ! ᴡʜɪᴄʜ ᴄᴀɴ ᴀᴜᴛᴏʀᴇɴᴀᴍᴇ ʏᴏᴜʀ ғɪʟᴇs ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ᴀɴᴅ ᴛʜᴜᴍʙɴᴀɪʟ ᴀɴᴅ ᴀʟsᴏ sᴇǫᴜᴇɴᴄᴇ ᴛʜᴇᴍ ᴘᴇʀғᴇᴄᴛʟʏ</b>"""
+    
+    HELP_TXT = """<b>Available commands for {mention}:
+
+» Send files or videos to auto-queue them for processing
+» /ssequence - Start a file sequence manually
+» /esequence - End and process the sequence
+» /setsticker - Set a custom sticker (reply to a sticker)
+» /getsticker - View your current sticker
+» /delsticker - Reset to default sticker
+» /mode - Set sorting mode (quality, title, both, episode)
+» /smode - Set sticker display mode (quality, default)
+» /setdump - Set your dump channel (e.g., /setdump -1001234567890)
+» /getdump - View your dump channel
+» /deldump - Remove your dump channel
+» /leaderboard - View top users by file count
+» /bought - Submit premium payment screenshot (reply to a photo)
+
+Admin commands:
+» /users - View total users
+» /ban - Ban a user (reply or provide ID)
+» /unban - Unban a user (reply or provide ID)
+» /banlist - List banned users
+» /addadmin - Add an admin (reply or provide ID)
+» /deladmin - Remove an admin (reply or provide ID)
+» /adminlist - List admins
+» /adminmode - Toggle admin mode
+
+Owner commands:
+» /setcompletesticker - Set completion sticker (reply to a sticker)</b>"""
+    
+    ABOUT_TXT = """<b>❍ ᴍʏ ɴᴀᴍᴇ : <a href="https://t.me/codeflix_bots">ᴀᴜᴛᴏ ʀᴇɴᴀᴍᴇ</a>
+❍ ᴅᴇᴠᴇʟᴏᴩᴇʀ : <a href="https://t.me/cosmic_freak">ʏᴀᴛᴏ</a>
+❍ ɢɪᴛʜᴜʙ : <a href="https://github.com/cosmic_freak">ʏᴀᴛᴏ</a>
+❍ ʟᴀɴɢᴜᴀɢᴇ : <a href="https://www.python.org/">ᴘʏᴛʜᴏɴ</a>
+❍ ᴅᴀᴛᴀʙᴀꜱᴇ : <a href="https://www.mongodb.com/">ᴍᴏɴɢᴏ ᴅʙ</a>
+❍ ʜᴏꜱᴛᴇᴅ ᴏɴ : <a href="https://t.me/codeflix_bots">ᴠᴘs</a>
+❍ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/animes_cruise">ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>
+
+➻ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ғᴏʀ ɢᴇᴛᴛɪɴɢ ʙᴀsɪᴄ ʜᴇʟᴩ ᴀɴᴅ ɪɴғᴏ ᴀʙᴏᴜᴛ ᴍᴇ.</b>"""
